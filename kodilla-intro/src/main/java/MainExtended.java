565ee9f63544c5f895e9db9d026f3e04a8cf5716
Weronika Gawronska <weronika.gawronska95@gmail.com>
2021-11-06 16:12:50 -0500

    Initial version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myName = "Weronika";
+        int myAge = 26;
+        double height = 1.70;
+        String birthPlace = "Zielona Góra";
+        int yearBirth = 1995;
+        char firstLetter = 'W';
+        System.out.println(myName);
+        System.out.println(myAge);
+        System.out.println(height);
+        System.out.println(birthPlace);
+        System.out.println(yearBirth);
+        System.out.println(firstLetter);
+    }
+}
